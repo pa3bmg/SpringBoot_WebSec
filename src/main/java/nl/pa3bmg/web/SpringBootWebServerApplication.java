500475b3b90c88b054e3cd788d2666801be69748
Ton Smink <pa3bmg@home.nl>
2018-02-09 10:54:22 +0100

    first commit

---
+++ src/main/java/nl/pa3bmg/web/SpringBootWebServerApplication.java
@@ -0,0 +1,12 @@
+package nl.pa3bmg.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebServerApplication.class, args);
+	}
+}
